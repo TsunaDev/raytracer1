d3930d9660664abed8b682ed1309f846d76ba708
martin_v <martin.van-elslande@epitech.eu>
2017-02-23 11:26:06 +0100

    lightfix

--- src/light.c
+++ src/light.c
@@ -5,7 +5,7 @@
 ** Login   <martin.van-elslande@epitech.eu>
 ** 
 ** Started on  Wed Feb 22 23:54:10 2017 Martin Van Elslande
-** Last update Thu Feb 23 11:16:58 2017 Martin Van Elslande
+** Last update Thu Feb 23 11:25:49 2017 Martin Van Elslande
 */
 
 #include	<SFML/Graphics.h>
@@ -16,12 +16,22 @@ float	get_light_coef(sfVector3f light_vector, sfVector3f normal_vector)
   float	coef;
   float	norm1;
   float	norm2;
+  float	vec1;
+  float	vec2;
 
+  vec1 = sqrt(powf(light_vector.x, 2.0f) + powf(light_vector.y, 2.0f) + powf(light_vector.z, 2.0f));
+  vec2 = sqrt(powf(normal_vector.x, 2.0f) + powf(normal_vector.y, 2.0f) + powf(normal_vector.z, 2.0f));
+  light_vector.x /= vec1;
+  light_vector.y /= vec1;
+  light_vector.z /= vec1;
+  normal_vector.x /= vec2;
+  normal_vector.y /= vec2;
+  normal_vector.z /= vec2;
   coef = light_vector.x * normal_vector.x + light_vector.y * normal_vector.y + light_vector.z * normal_vector.z;
   norm1 = sqrt(powf(light_vector.x, 2.0f) + powf(light_vector.y, 2.0f) + powf(light_vector.z, 2.0f));
   norm2 = sqrt(powf(normal_vector.x, 2.0f) + powf(normal_vector.y, 2.0f) + powf(normal_vector.z, 2.0f));
   coef /= norm1 * norm2;
-  if (coef > 0)
+  if (coef > 0.0f)
     return (coef);
   return (0.0f);
 }
--- src/rotate.c
+++ src/rotate.c
@@ -5,7 +5,7 @@
 ** Login   <martin.van-elslande@epitech.eu>
 ** 
 ** Started on  Sat Feb 18 20:04:53 2017 Martin Van Elslande
-** Last update Thu Feb 23 11:15:00 2017 Martin Van Elslande
+** Last update Thu Feb 23 11:20:37 2017 Martin Van Elslande
 */
 
 #include	<SFML/Graphics.h>
@@ -15,9 +15,9 @@ sfVector3f	rotate_xyz(sfVector3f to_rotate, sfVector3f angles)
 {
   sfVector3f	rotated;
 
-  angle.x *= M_PI / 180.0f;
-  angle.y *= M_PI / 180.0f;
-  angle.z *= M_PI / 180.0f;
+  angles.x *= M_PI / 180.0f;
+  angles.y *= M_PI / 180.0f;
+  angles.z *= M_PI / 180.0f;
   rotated.x = to_rotate.x;
   rotated.y = to_rotate.y * cos(angles.x) + to_rotate.z * (-sin(angles.x));
   rotated.z = to_rotate.y * sin(angles.x) + to_rotate.z * cos(angles.x);
@@ -34,9 +34,9 @@ sfVector3f	rotate_zyx(sfVector3f to_rotate, sfVector3f angles)
 {
   sfVector3f	rotated;
 
-  angle.x *= M_PI / 180.0f;
-  angle.y *= M_PI / 180.0f;
-  angle.z *= M_PI / 180.0f;
+  angles.x *= M_PI / 180.0f;
+  angles.y *= M_PI / 180.0f;
+  angles.z *= M_PI / 180.0f;
   rotated.x = to_rotate.x * cos(angles.z) + to_rotate.y * (-sin(angles.z));
   rotated.y = to_rotate.x * sin(angles.z) + to_rotate.y * cos(angles.z);
   rotated.z = to_rotate.z;
