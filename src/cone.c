923ca70b89836ad29409e5805f35f8dc683d2595
martin_v <martin.van-elslande@epitech.eu>
2017-02-23 11:52:09 +0100

    conetest

--- src/cone.c
+++ src/cone.c
@@ -5,7 +5,7 @@
 ** Login   <martin.van-elslande@epitech.eu>
 ** 
 ** Started on  Wed Feb 22 22:24:14 2017 Martin Van Elslande
-** Last update Thu Feb 23 00:04:51 2017 Martin Van Elslande
+** Last update Thu Feb 23 11:52:01 2017 Martin Van Elslande
 */
 
 #include	<SFML/Graphics.h>
@@ -24,6 +24,8 @@ float	intersect_cone(sfVector3f eye_pos, sfVector3f dir_vector, float semiangle)
   b = 2.0f * (dir_vector.x * eye_pos.x + dir_vector.y * eye_pos.y - ((dir_vector.y * eye_pos.y) / powf(tan((90.0f - semiangle) * M_PI / 180.0f), 2.0f)));
   c = powf(eye_pos.x, 2.0f) + powf(eye_pos.y, 2.0f) - (powf(eye_pos.z, 2.0f) / powf(tan((90.0f - semiangle) * M_PI / 180.0f), 2.0f));
   delta = powf(b, 2.0f) - 4.0f * a * c;
+  if (a == 0 && b == 0)
+    return (0.0f);
   if (delta < 0)
     return (-1.0f);
   else if (delta == 0)
